d166887f08a83cb652de1a9fac2b33995bab0fe8
Jorge Ubirajara Pedreira Junior <jorgesars@gmail.com>
2024-09-24 19:05:14 -0300

    feat: adiciona plot do mapa de fluxos com ggsave na pasta vignettes

--- vignettes/testes.R
+++ vignettes/testes.R
@@ -3,7 +3,6 @@ library(sf); library(tidyverse)
 # Carrega as bases de dados
 load("data/tables/tabelas_dados.rda")
 load("data/geo/zonas_rms_pol_crs4674.rda")
-load("data/geo/zonas_rms_pts_crs4674.rda")
 load("data/geo/subzonas_rms_pol_crs4674.rda")
 
 # Armazena base de dados de viagens e arquivos de polígonos das zonas e subzonas
@@ -23,35 +22,4 @@ sum(zonas_unicas %in% pol_zon$Name) # 217 zonas presentes no arquivo de polígon
 subzonas_unicas <- union(unique(viagens$ZONAsub_O),unique(viagens$ZONAsub_D))
 length(subzonas_unicas) # 842
 sum(subzonas_unicas %in% pol_subzon$Name) # 802 subzonas presentes no arquivo de polígonos
-
-# Produzindo dados de fluxos de viagens 
-pts_zon <- zonas_rms_pts_crs4674
-pts_zon <- cbind(pts_zon,st_coordinates(pts_zon))
-
-flux <- viagens %>% 
-  group_by(Z_O,Z_D) %>% 
-  summarize(n = sum(FATOR_EXP, na.rm = T)) %>% 
-  left_join(pts_zon %>% st_drop_geometry() %>% select(Name,X,Y),
-            by = join_by(Z_O == Name)) %>% 
-  rename('O_X' = 'X', 'O_Y' = 'Y') %>% 
-  left_join(pts_zon %>% st_drop_geometry() %>% select(Name,X,Y),
-            by = join_by(Z_D == Name)) %>% 
-  rename('D_X' = 'X', 'D_Y' = 'Y')
-
-# Mapa de fluxos OD (somente acima de 2000 viagens)
-ggplot() +
-  geom_sf(data = pol_zon, fill = "#353941", color = "white", linewidth = 0.05) +
-  geom_curve(data = flux %>% filter(!is.na(Z_O) & !is.na(Z_D) &
-                                                 Z_O != Z_D & n >= 2000),
-             aes(x = O_X, y = O_Y,
-                 xend = D_X, yend = D_Y,
-                 linewidth = n),
-             arrow = arrow(length = unit(0.01, "npc"), type = 'closed'),
-             color = '#E8772D', alpha = 0.8) +
-  theme_void() +
-  theme(
-    plot.title = element_text(hjust = 0.5),
-    plot.subtitle = element_text(hjust = 0.5)
-  )
-
 
